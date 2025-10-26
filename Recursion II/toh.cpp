ec190ef91e1d687e8cb201991d9476ad791ec965
Apoorv Saxena <154752694+Apoorv-Saxena08@users.noreply.github.com>
2025-10-26 19:30:26 +0530

    Add Tower of Hanoi solution in toh.cpp
    Implement Tower of Hanoi algorithm using recursion.

---
+++ Recursion II/toh.cpp
@@ -0,0 +1,17 @@
+class Solution{
+  public:
+    int towerOfHanoi(int n, int from, int to, int aux) {
+        if (n == 1) {
+            cout << "move disk " << n << " from rod " << from << " to rod " << to << endl;
+            return 1;
+        }
+        
+        int moves = 0;
+        moves += towerOfHanoi(n - 1, from, aux, to);
+        cout << "move disk " << n << " from rod " << from << " to rod " << to << endl;
+        moves += 1;
+        moves += towerOfHanoi(n - 1, aux, to, from);
+        
+        return moves;
+    }
+};
