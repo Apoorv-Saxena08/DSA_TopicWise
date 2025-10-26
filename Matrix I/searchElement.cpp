d707e23725d9ca984932e5f116c834350384d6c4
Apoorv Saxena <154752694+Apoorv-Saxena08@users.noreply.github.com>
2025-10-26 19:32:40 +0530

    Implement binary search in a 2D matrix

---
+++ Matrix I/searchElement.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>> matrix, int target) {
+        int n = matrix.size() ;
+        int m = matrix[0].size();
+
+
+        int start=0 , end = n*m-1;
+        while(start<=end){
+            int mid= start + (end-start)/2;
+            int row_idx = mid/m;
+            int col_idx = mid%m;
+            if(matrix[row_idx][col_idx]==target){
+                return 1;
+            }
+            else if(matrix[row_idx][col_idx]<target){
+                start = mid+1;
+            }
+            else{
+                end = mid-1;
+            }
+        }
+        return 0;
+    }
+};
