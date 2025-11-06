b32b9cb70c2f849363a7f11cf675ca2b308c4bff
Apoorv Saxena <154752694+Apoorv-Saxena08@users.noreply.github.com>
2025-11-07 00:56:26 +0530

    Create buildQuery&Sum.cpp

---
+++ Segment Tree I/buildQuery&Sum.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+ 
+int seg[400];
+ 
+void build(int arr[], int index, int low, int high) {
+	if(low == high) {
+    	seg[index] = arr[low];
+    	return;
+	}
+	int mid = (low + high) / 2;
+	build(arr, 2*index+1, low, mid);
+	build(arr, 2*index+2, mid+1, high);
+	seg[index] = seg[2*index+1] + seg[2*index+2];
+}
+ 
+int query(int index, int low, int high, int l, int r) {
+	if(r < low || l > high) return 0;
+	if(l <= low && high <= r) return seg[index];
+	int mid = (low + high) / 2;
+	return query(2*index+1, low, mid, l, r) + query(2*index+2, mid+1, high, l, r);
+}
+ 
+int main() {
+	int n;
+	cin >> n;
+	int arr[100];
+	for(int i = 0; i < n; i++) cin >> arr[i];
+	build(arr, 0, 0, n-1);
+	int l, r;
+	cin >> l >> r;
+	cout << "Sum: " << query(0, 0, n-1, l, r);
+	return 0;
+}
