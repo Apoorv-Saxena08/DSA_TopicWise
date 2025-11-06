1adcc10073f91798ae7d9996678404f04c505c45
Apoorv Saxena <154752694+Apoorv-Saxena08@users.noreply.github.com>
2025-11-07 01:00:17 +0530

    Create 2sum.cpp

---
+++ Advance Array I/2sum.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int n, target; cin>>n;
+	int a[100];
+	for(int i=0;i<n;i++) cin>>a[i];
+	for(int i=0;i<n;i++){
+    	for(int j=i+1;j<n;j++){
+        	if(a[i]+a[j]==target){ cout<<i<<" "<<j; return 0; }
+    	}
+	}
+	cout<<"No pair";
+	return 0;
+}
